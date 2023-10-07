87282c6cd1dbd9dc6d36947a2f89ae1f0b599678
Sivateja <57987686+sivatejaa@users.noreply.github.com>
2023-10-06 19:36:45 -0500

    Added Login and Signup Fragments - Sivateja Raju Thangala

--- StayEasy/app/src/main/java/com/example/stayeasy/LoginTabFragment.java
+++ StayEasy/app/src/main/java/com/example/stayeasy/LoginTabFragment.java
@@ -1,5 +1,6 @@
 package com.example.stayeasy;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class LoginTabFragment extends Fragment {
@@ -14,7 +16,8 @@ public class LoginTabFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_tab, container, false);
+        View view=inflater.inflate(R.layout.fragment_login_tab, container, false);
+
+        return view;
     }
 }

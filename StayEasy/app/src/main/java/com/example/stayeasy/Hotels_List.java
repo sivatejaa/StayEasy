96050a2ecb16a955fd747c0f35fe0e81d4960605
Anuradha Gudimetla <122694859+agudimet@users.noreply.github.com>
2023-11-17 23:44:01 -0600

    Added new changes
    new change of added images foe motels

--- StayEasy/app/src/main/java/com/example/stayeasy/Hotels_List.java
+++ StayEasy/app/src/main/java/com/example/stayeasy/Hotels_List.java
@@ -23,9 +23,9 @@ public class Hotels_List extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotels_list);
 
-        hotels.add(new Hotel( R.drawable.hotel,"Holiday Inn", "5 Hotel Chains with Two-Bedroom Suites You Can Book."));
-        hotels.add(new Hotel(R.drawable.hotel1,"Haytt", "2 bed 1 bath Super Deluxe Hotel 2 Star Hotel"));
-        hotels.add(new Hotel(R.drawable.hotel2,"The Plaza", "3 bed 1 bath Hotel 3 Star Hotel with a wide range of Facilities" ));
+        hotels.add(new Hotel( R.drawable.motel_2,"Holiday Inn", "5 Hotel Chains with Two-Bedroom Suites You Can Book."));
+        hotels.add(new Hotel(R.drawable.img,"Haytt", "2 bed 1 bath Super Deluxe Hotel 2 Star Hotel"));
+        hotels.add(new Hotel(R.drawable.motel_4,"Skyline", "3 bed 1 bath Hotel 3 Star Hotel with a wide range of Facilities" ));
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
